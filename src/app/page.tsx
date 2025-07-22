82ac70c4ece697c3914d4a2e96b3dff26a7ca92f
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-07-22 23:33:47 +0530

    background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import { getQueryClient, trpc } from "@/trpc/server";
-import {Client} from "./client";
-
-const Page= async ()=>{
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"Tejas yupp"}));
+import { Button } from "@/components/ui/button"
+import {useTRPC} from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query"
+const Page=  ()=>{
+  const trpc= useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return(
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Client/>
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7x1 mx-auto">
+      <Button onClick={()=> invoke.mutate({text:"tejas" })}>
+        Invoke background job
+      </Button>
+    </div>
   )
 }
