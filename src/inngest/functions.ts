bfe9030fb051b17c6748c0fce1d41fd7dc8d499f
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-07-25 23:55:09 +0530

    a small commit

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,8 +1,11 @@
-import {   openai, createAgent } from "@inngest/agent-kit";
+import {   openai, createAgent, createTool } from "@inngest/agent-kit";
 import { inngest } from "./client";
 import {Sandbox} from "@e2b/code-interpreter"
 import { stepsSchemas } from "inngest/api/schema";
 import { getSanbox } from "./utils";
+import { Command } from "lucide-react";
+import { stderr, stdout } from "process";
+import z from "zod";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -18,6 +21,38 @@ export const helloWorld = inngest.createFunction(
       name: "summarizer",
       system: "You are an expert summarizer.  You summarize in 2 words",
       model: openai({ model: "gpt-4o"}),
+      tools: [
+        createTool({
+          name: "terminal",
+          description: "use this terminal to run commands",
+          parameters : z.object({
+            command: z.string(),
+          }),
+          handler: async ({command}, {step})=> {
+            return await step?.run ("terminal", async()=>{
+              const buffers= {stdout: "", stderr:""};
+
+              try {
+                const sandbox= await getSanbox(sandboxId);
+                 const result= await sandbox.commands.run(command, {
+                  onStdout: (data: String )=> {
+                    buffers.stdout += data;
+                  },
+                  onStderr: (data : string )=> {
+                    buffers.stderr+= data;
+                  }
+                 });
+                 return result.stdout;
+              }catch(e){
+                console.error(
+                  `Command failed: ${e} \nstdout: ${buffers.stdout}\nstderror:${buffers.stderr}`,
+                );
+                return `Command failed: ${e} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr} `;
+              }
+            })
+          }
+        })
+      ]
     }); 
 
     const { output } = await summarizer.run(
