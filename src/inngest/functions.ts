bd415e083f547c6edbcc0dbf9229c882bed26b32
Tejasva Kant Saini <tsaini0408@gmail.com>
2025-07-26 23:35:07 +0530

    some more commits

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,9 +3,13 @@ import { inngest } from "./client";
 import {Sandbox} from "@e2b/code-interpreter"
 import { stepsSchemas } from "inngest/api/schema";
 import { getSanbox } from "./utils";
-import { Command } from "lucide-react";
+import { Command, Files } from "lucide-react";
 import { stderr, stdout } from "process";
-import z from "zod";
+import {z, ZodType} from "zod";
+import { Content } from "vaul";
+import type { AnyZodType } from "@inngest/agent-kit";
+import { handler } from "next/dist/build/templates/app-page";
+
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
@@ -49,9 +53,71 @@ export const helloWorld = inngest.createFunction(
                 );
                 return `Command failed: ${e} \nstdout: ${buffers.stdout}\nstderror: ${buffers.stderr} `;
               }
-            })
-          }
-        })
+            });
+          },
+          }),
+          createTool({
+            name: "createupdatefiles",
+            description: "Create or update files in the sandbox",
+            parameters: z.object ({
+              files: z.array(
+                z.object({
+                  path: z.string(),
+                  content: z.string(),
+                }),
+              ),
+              
+            }),
+           handler: async(
+            {files}, 
+            {step, network}
+           )=>{
+            const newfiles= await step?.run("createOrUpdateFiles", async()=>{
+              try {
+                const updatedfiles= network.state.data.files || {};
+                const sandbox= await getSanbox(sandboxId);
+                for (const file of files) {
+                  await sandbox.files.write(file.path, file.content);
+                  updatedfiles[file.path]= file.content;
+                }
+                return updatedfiles;
+              } catch (e) {
+                return "Error:"+e;
+                
+              }
+            });
+
+             if(typeof newfiles === "object"){
+              network.state.data.files = newfiles;
+              
+             }
+
+           }
+          
+        }),
+         
+        createTool({
+          name: "readFiles",
+          description: "Read files from the sandbox",
+          parameters: z.object(z.string()),
+
+        }),
+         handler: async ({files}, {steps})=>{
+          return await step?.run("readFiles", async()=>{
+            try {
+              const sandbox= await getSanbox(sandboxId);
+              const contents= [];
+              for (const file of files){
+                const content = await sandbox.files.read(file);
+                contents.push({path: file, content});
+              }
+              return JSON.stringify(contents);
+            } catch (e) {
+              return "Error: "+ e;
+              
+            }
+          })
+         }, 
       ]
     }); 
 
